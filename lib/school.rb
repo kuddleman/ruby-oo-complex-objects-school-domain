4f4a6a049203566fb9e0856d816e59be8ac5c32d
Donny <edl1155@gmail.com>
2019-12-11 10:36:30 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+
+class School
+  attr_reader :name
+  def initialize(name, roster={})
+    @name = name
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    # does grade/key already exist?
+    # then push name to existing array
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name   
+    end  
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.each do | grade, name_array |
+      new_hash[grade] = name_array.sort
+    end
+    new_hash
+  end
+
+end
